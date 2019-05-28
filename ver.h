42ef1a194ef61da21e97b259b3bb3239c4c5387f
dbezrukov <dmitry.bezrukov@aquantia.com>
2019-05-28 12:28:49 +0300

    Bump version

--- ver.h
+++ ver.h
@@ -15,6 +15,6 @@
 #define NIC_BUILD_DRIVER_VERSION           3
 #define NIC_REVISION_DRIVER_VERSION        0
 
-#define AQ_CFG_DRV_VERSION_SUFFIX "-esxi1.5"
+#define AQ_CFG_DRV_VERSION_SUFFIX "-esxi1.6"
 
 #endif /* VER_H */
